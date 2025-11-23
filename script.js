0719b7137bcd1b3eb43ced42c66c16ac1fa44c05
ManikMagarSir <np03cy4s250029@heraldcollege.edu.np>
2025-11-23 20:21:26 +0545

    uhh forms and etc

--- script.js
+++ script.js
@@ -5,3 +5,54 @@ setInterval(() => {
     }, 1000);
     manik.style.color = 'White';
 }, 2000);
+let ex = document.querySelector('.ex');
+let ham = document.getElementById('hamburger');
+let navUl = document.querySelector('nav ul');
+    ham.addEventListener('click', function() {
+    if (navUl.style.display === 'flex') {
+        navUl.style.display = 'none';
+        ex.style.display = 'none';
+    }
+    else {
+        navUl.style.display = 'flex';
+        ham.style.display = 'none';
+        ex.style.display = 'flex';
+    }
+});
+
+    ex.addEventListener('click', function() {
+    if (navUl.style.display === 'flex') {
+        navUl.style.display = 'none';
+        ex.style.display = 'none';
+        ham.style.display = 'flex';
+    }
+});
+
+let button = document.querySelector('.btn');
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let message = document.getElementById('message');
+let hehe = document.querySelector('.hehe');
+let msg = document.querySelector('.msg');
+let form = document.querySelector('form');
+    form.addEventListener('submit', function(event) {
+        if (name.value === '' || email.value === '' || message.value === '') {
+            event.preventDefault();
+            msg.style.display = 'flex';
+            hehe.style.color = 'red';
+            hehe.textContent = 'Please fill in all fields.';
+            setTimeout(() => {
+                msg.style.display = 'none';
+            }, 1000);
+        }
+        else {
+            event.preventDefault();
+            msg.style.display = 'flex';
+            hehe.style.color = 'black';
+            hehe.textContent = 'Thank you for your message! I will touch u shortly.';
+            setTimeout(() => {
+                msg.style.display = 'none';
+                form.reset();
+            }, 1500);
+        }
+    });
